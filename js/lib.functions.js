140d689b0016f1ff4803fdca562289671a981455
dhub01 <PidProduction@Pids-MacBook-Pro.local>
2014-06-26 15:17:04 +0800

    NewsFeed Test

---
+++ js/lib.functions.js
@@ -0,0 +1,65 @@
+
+
+function openDashWindow(cls, modal){
+    var acls = '';
+    
+    if(modal === 0){
+        acls = 'oLayer'; //modal window
+        close= "onclick='closeDashWindow(\""+cls+"\");'";
+    } else {
+        acls = 'nLayer';
+        close= "";
+    }
+
+    var zIndex  = getHighestZIndex();
+    
+    var xInd = parseFloat(zIndex) + 100;
+    var yInd = parseFloat(zIndex) + 50;
+
+    $("."+cls).css('z-index',xInd).fadeIn();
+    
+    var divElem = $("<div id='"+cls+"overlay' class='overlayContainer "+acls+"' "+close+" style='z-index:"+yInd+"'></div>");
+    divElem.css('z-index',yInd).appendTo('body').fadeIn();
+}
+
+function openModalWindow(cls){
+    var zIndex  = getHighestZIndex();
+    
+    var xInd = parseFloat(zIndex) + 100;
+    var yInd = parseFloat(zIndex) + 50;
+    
+    $("."+cls).css('z-index',xInd).fadeIn();
+    
+    var divElem = $("<div id='"+cls+"overlay' class='overlayContainer oLayer'></div>");
+    divElem.css('z-index',yInd).appendTo('body').fadeIn();
+}
+
+function getHighestZIndex(){
+    var maxZ = Math.max.apply(null,$.map($('body > *'), function(e,n){
+           if($(e).css('position') == 'absolute')
+                return parseInt($(e).css('z-index'))||1 ;
+           })
+    );
+    return maxZ;
+}
+
+function closeDashWindow(cls){
+    $("."+cls+", #"+cls+"overlay").hide();
+}
+
+function closeTagChildrenWindows(){
+    var divElems = ".tagChildrenDropDown, .tagChildrenWindow, #tagChildrenDropDownoverlay, #tagChildrenWindowoverlay";
+    $(divElems).hide();
+}
+
+function selectArticle(param){
+    var varF = $(param).val();
+ 
+    if(varF == 'families'){
+        openModalWindow('selectFamiliesWindow');
+    } else {
+        openModalWindow('selectRoomsWindow');
+    }
+}
+
+
